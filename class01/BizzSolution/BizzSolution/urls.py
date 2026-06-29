cd909d6d548470032d3403a016dee691ae264caf
Sohag <shshohagh4@gmail.com>
2026-06-30 03:30:00 +0600

    Move views to StudentService app

--- class01/BizzSolution/BizzSolution/urls.py
+++ class01/BizzSolution/BizzSolution/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 # from django.shortcuts import HttpResponse
-from BizzSolution.views import home, about
+from StudentService.views import home, about
 
 urlpatterns = [
     path('admin/', admin.site.urls),
